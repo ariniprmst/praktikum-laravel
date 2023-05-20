3a6016be12b6b792dfb2d6ceb6ad5d4473f30058
Arini <arini.pramesta.20@student.is.ittelkom-sby.ac.id>
2023-05-20 09:50:48 +0700

    change indent

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,13 +10,15 @@
     <title>Welcome</title>
     @vite('resources/sass/app.scss')
 </head>
+
 <body>
     <div class="container m-5">
 
-    {{-- Contoh cara mereferensikan gambar di dalam file blade dengan
+        {{-- Contoh cara mereferensikan gambar di dalam file blade dengan
 menggunakan pendekatan Vite --}}
         <img class="img-thumbnail" src="{{Vite::asset('resources/images/main.svg') }}" alt="image">
     </div>
     @vite('resources/js/app.js')
 </body>
+
 </html>
